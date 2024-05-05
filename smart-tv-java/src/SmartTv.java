418a8ba3da2c24fe90a969737033dc9aafef0e08
Thyago <thy_thyago2.5@hotmail.com>
2024-05-04 22:23:54 -0300

    Adicionando comentários Java. Refinando a organização do código

--- smart-tv-java/src/SmartTv.java
+++ smart-tv-java/src/SmartTv.java
@@ -1,33 +1,46 @@
+/** 
+ * @author Thyago
+ * @since 2024
+ */
+
+/** 
+    * Esse é o código de uma lógica de uma TV genérica. Conforme o autor for progredindo em seu aprendizado, mais refinado o código será! 
+    */
 public class SmartTv {
+    
     boolean onOff = false;
     int channel = 0;
     int volume = 0;
-
+     /** 
+    * Aqui eu ligo ou desligo a tv.
+    */
     public void powerOnOff(){
         onOff = !onOff;
         String status = onOff ? "Ligando" : "Desligando";
         System.out.println(status + " Tv");
     }
 
+    /**Aumento o volume */
     public void volumeUp(){
         volume++;
         System.out.println("Aumentando volume " + volume);
     }
+    /**Abaixo o volume */
     public void volumeDown(){
         volume--;
         System.out.println("Diminuindo volume " + volume);
     }
-
+    /** Passo o canal em ordem crescente de 1 em 1 */
     public void channelUp(){
         channel++;
         System.out.println("Mudando o canal para " + channel);
     }
-
+     /** Passo o canal em ordem decrescente de 1 em 1 */
     public void channelDown(){
         channel--;
         System.out.println("Mudando o canal para " + channel);
     }
-
+    /**Altero o canal para um outro qualquer */
     public void changeChannel(int newChannel){
         channel = newChannel;
         System.out.println("Mudando para o canal " + channel);
--- smart-tv-java/src/User.java
+++ smart-tv-java/src/User.java
@@ -1,28 +1,32 @@
+/**Classe que simula um usuário interagindo com a tv */
 public class User {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         SmartTv smartTv = new SmartTv();
         int i = 0;
-
+        /**Ligo ou desligo a TV 
+         * A TV inicia desligada, logo, na primeira chamada ela é ligada.
+         * Por fim, a próxima chamada deste método desliga a TV no fim do código.
+        */
         smartTv.powerOnOff();
-
+        /**Laço para passar 5 canais em ordem crescente */
         for(i=0; i<5; i++){
             smartTv.channelUp();
         }
-
+        /****Laço para voltar 2 canais */
         for(i=0; i<2; i++){
             smartTv.channelDown();
         }
-       
+       /**Canal alterado para o 37 */
         smartTv.changeChannel(37);
-        
+        /**Aumentando o volume em +5 */
         for(i=0; i<5; i++){
             smartTv.volumeUp();
         }
-
+        /**Diminuindo o volume em -2 */
         for(i=0; i<2; i++){
             smartTv.volumeDown();
         }
-        
+
         smartTv.powerOnOff();
     }
 }
